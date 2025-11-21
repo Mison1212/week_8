b83131e0b9a12a157df85af5a821cfbcfa0d6029
Mison1212 <misonwenda20@gmail.com>
2025-11-21 09:43:27 +0700

    make column for text title

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -17,18 +17,51 @@ class MyApp extends StatelessWidget {
                 children: [
                   Card(
                     color: Colors.red,
-                    child: Text("text",
+                    child:Text("Test",
                       style: TextStyle(
                         fontSize: 16.0,
                       ),
-                    ),
 
-                  )
+                    ),
+                  ),
+                   SizedBox(height: 5 ),
+
+                   Card(
+                    elevation: 8,
+                    child: Padding(
+                      //padding
+                      padding: const EdgeInsets.all(20.0),
+                      //membuat Column  untuk menempatkan text title 
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("About me",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 5,),
+
+                          Text("BALI, KOMPAS.com – Seorang turis asal China meninggal dunia setelah mengalami muntah hebat dan menggigil di sebuah akomodasi murah di kawasan Canggu, Bali.",
+                            style: TextStyle(
+                              fontSize: 12.0,
+                            ),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ],
+                
+
+                      ),
+                    ),
+                   )
+
                 ],
               ),
             ),
           ),
-        );     
+        );
           
   }
 }
